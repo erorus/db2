a5509865b1267553ff252e357f0cc61285a47019
Erorus <github@theunderminejournal.com>
2017-04-10 16:39:21 -0400

    satisfy PHPUnit

--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -2,7 +2,7 @@
 
 use Erorus\DB2\Reader;
 
-class ReaderTest extends PHPUnit_Framework_TestCase
+class ReaderTest extends \PHPUnit\Framework\TestCase
 {
     const WDB2_PATH = __DIR__.'/wdb2';
     const WDB5_PATH = __DIR__.'/wdb5';
